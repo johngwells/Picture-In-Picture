23885da0d57daccf4e933ed10e9f3d96eb9b170f
John Wells <molekewl2017@gmail.com>
2020-08-13 17:36:16 -0700

    web page is able to prompt user to share screen

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const button = document.getElementById('button');
 // Prompt user to select media stream, pass to video element, then play
 async function selectMediaStream() {
   try {
-
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.onplay();
+    }
   } catch(error) {
     console.log('Whoops', error);
   }

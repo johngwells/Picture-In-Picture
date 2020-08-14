113909ffc02eb084334b6aa6d33c0e90fc3bd266
John Wells <molekewl2017@gmail.com>
2020-08-13 17:31:28 -0700

    create document.getElementById and async function for mediaStream

---
+++ script.js
@@ -0,0 +1,14 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+
+  } catch(error) {
+    console.log('Whoops', error);
+  }
+}
+
+// On Load
+selectMediaStream();
